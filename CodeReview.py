4e2bc4725ff8a2b2e04fa93929874923210bf41b
Aaron Greene <pashonic@gmail.com>
2012-07-04 23:35:58 -0700

    Some work

--- CodeReview.py
+++ CodeReview.py
@@ -1,1 +1,23 @@
-print ('CodeReview')
+import os
+import sys
+import fnmatch
+
+files2Process = []
+
+def AddFilesFromDirectory (directoryPath):
+	for root, dirnames, filenames in os.walk(directoryPath):
+		for filename in fnmatch.filter(filenames, '*.py'):
+			files2Process.append (os.path.join(root, filename))
+
+for arg in sys.argv[1:]:
+	if os.path.isdir (arg):
+		AddFilesFromDirectory (directoryPath)
+	else:
+		files2Process.append (arg)
+
+if (len (sys.argv) <= 1):
+	AddFilesFromDirectory ('.')
+
+
+for i in files2Process:
+	print i
