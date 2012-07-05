10ee48036f382cd8a53e021baa92fbb3fb55324d
Aaron Greene <pashonic@gmail.com>
2012-07-05 00:44:05 -0700

    Some work

--- CodeReview.py
+++ CodeReview.py
@@ -3,11 +3,21 @@ import sys
 import fnmatch
 
 files2Process = []
-
 def AddFilesFromDirectory (directoryPath):
-	for root, dirnames, filenames in os.walk(directoryPath):
-		for filename in fnmatch.filter(filenames, '*.py'):
-			files2Process.append (os.path.join(root, filename))
+	for root, dirnames, filenames in os.walk (directoryPath):
+		for filename in fnmatch.filter (filenames, '*.py'):
+			
+			#
+			# Ignore the this script file.
+			#
+			
+			if sys.argv[1] in filename:
+				continue
+			files2Process.append (os.path.join (root, filename))
+
+#
+# Process arguments.
+#
 
 for arg in sys.argv[1:]:
 	if os.path.isdir (arg):
@@ -15,9 +25,19 @@ for arg in sys.argv[1:]:
 	else:
 		files2Process.append (arg)
 
-if (len (sys.argv) <= 1):
+#
+# Add working directory by default if no arguments were given.
+#
+
+if (len (files2Process) == 0):
 	AddFilesFromDirectory ('.')
 
+#
+# Process files.
+#
 
-for i in files2Process:
-	print i
+for filePath in files2Process:
+	fileContents = open (filePath).read ()
+	
+def RemoveWhiteSpace (fileContents)
+	return 
