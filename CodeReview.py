0686ade3bab446e331eed13e926b6df53724c812
Aaron Greene <pashonic@gmail.com>
2012-07-06 00:56:30 -0700

    Change

--- CodeReview.py
+++ CodeReview.py
@@ -1,16 +1,17 @@
 import os
 import sys
 import fnmatch
+import inspect
 
 files2Process = []
 def AddFilesFromDirectory (directoryPath):
 	for root, dirnames, filenames in os.walk (directoryPath):
 		for filename in fnmatch.filter (filenames, '*.py'):
-			
+
 			#
-			# Ignore the this script file.
+			# Ignore this script file.
 			#
-			
+
 			if sys.argv[1] in filename:
 				continue
 			files2Process.append (os.path.join (root, filename))
@@ -33,11 +34,21 @@ if (len (files2Process) == 0):
 	AddFilesFromDirectory ('.')
 
 #
+# Defined actions.
+#
+
+class Actions:
+	def RemoveWhiteSpace (self, fileContents):
+		return re.replace (fileContents, 
+
+#
 # Process files.
 #
 
+actions = Actions ()
 for filePath in files2Process:
+	print 'Processing File: ' + filePath
 	fileContents = open (filePath).read ()
-	
-def RemoveWhiteSpace (fileContents)
-	return 
+	for name, method in inspect.getmembers (actions, callable):
+		print 'Running Action: ' + name
+		fileContents = method (fileContents)
