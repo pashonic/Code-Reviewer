3a8551745cac19d7c2397f95567a4a12e04d0d3c
Aaron Greene <pashonic@gmail.com>
2012-07-13 01:10:09 -0700

    Changes.

--- CodeReview.py
+++ CodeReview.py
@@ -3,82 +3,74 @@ import sys
 import fnmatch
 import inspect
 import re
+import sys
 
 files2Process = []
 def AddFilesFromDirectory (directoryPath):
-	for root, dirnames, filenames in os.walk (directoryPath):
-		for filename in fnmatch.filter (filenames, '*.py'):
+    for root, dirnames, filenames in os.walk (directoryPath):
+        for filename in fnmatch.filter (filenames, '*.py'):
 
-			#
-			# Ignore this script file.
-			#
+            #
+            # Ignore this script file.
+            #
 
-			if sys.argv[1] in filename:
-				continue
-			files2Process.append (os.path.join (root, filename))
+            if sys.argv[1] in filename:
+                continue
+            files2Process.append (os.path.join (root, filename))
 
 #
 # Process arguments.
 #
 
+Fix = False
 for arg in sys.argv[1:]:
-	if os.path.isdir (arg):
-		AddFilesFromDirectory (directoryPath)
-	else:
-		files2Process.append (arg)
+    if re.match ('fix\=(true|1)', arg.lower ()):
+        Fix = True
+        continue
+    if os.path.isdir (arg):
+        AddFilesFromDirectory (directoryPath)
+    else:
+        files2Process.append (arg)
 
 #
 # Add working directory by default if no arguments were given.
 #
 
 if (len (files2Process) == 0):
-	AddFilesFromDirectory ('.')
+    AddFilesFromDirectory ('.')
 
-def GetFileLines (fileContents):
-	return re.split ('\r\n|\r|\n', fileContents)
+#
+# Helper functions.
+#
 
-def RemoveQuotes (string):
-	string = re.sub ('\\\\[\'\"]', '', string)
-	string = re.sub ('[\"\'].*?[\"\']', '', string)
-	return string
+def GetFileLines (text):
+    return re.split ('\r\n|\r|\n', text)
+
+def RemoveQuotes (text):
+    quoteList = []
+    for count, quote in enumerate (re.finditer ('(?<!\\\\)(?P<quote>[\"\']).*?(?<!\\\\)(?P=quote)', text)):
+        capture = quote.group (0)
+        replace = re.sub ('.', chr (234), quote.group (0))
+        text = text[:quote.start (0)] + replace + text[quote.end (0):]
+        quoteList.append (capture)
+    return (text, quoteList)
+
+def RestoreQuotes (text, quoteList):
+    for quote in quoteList:
+        text = re.sub ('{0}+'.format (chr (234)), quote, text, 1)
+    return text
 
 #
 # Defined actions.
 #
 
 class Actions:
-	
-	def ReplaceTabsWithSpaces (self, fileContents):
-		return re.sub ('\t', ' ' * 4, fileContents)
 
-	def RemoveWhiteSpace (self, fileContents):
-		return re.sub ('[ \t]+(?=\r|\n|(\r\n)|\Z)', '', fileContents)
+    def Tabs (self, text):
+        return re.sub ('\t', ' ' * 4, text)
 
-	def LineCheck (self, fileContents):
-		
-		#
-		# Declare line issues.
-		#
-		
-		issues = [('No Parenthesis Space', '[^\s\(]\(', [RemoveQuotes]),
-		          ('Double Quote',         '[^\\\\]?\".*?[^\\\\]?\"', [])]
-		
-		#
-		# Find line issues.
-		#
-		
-		for lineNumber, lineString in enumerate (GetFileLines (fileContents)):
-			for name, reg, extraWorks in issues:
-				lineStringTemp = str (lineString)
-				for extraWork in extraWorks:
-					lineStringTemp = extraWork (lineStringTemp)
-				match = re.search (reg, lineStringTemp)
-				if match:
-					print 'Warning [{0}, {1}]: {2}: {3}'.format (lineNumber + 1,
-															     lineStringTemp.find (match.group (0)) + 1,
-															     name,
-															     match.group (0))
-		return fileContents
+    def WhiteSpace (self, text):
+        return re.sub ('[ \t]+(?=\r|\n|(\r\n)|\Z)', '', text)
 
 #
 # Process files.
@@ -87,25 +79,41 @@ class Actions:
 actions = Actions ()
 for filePath in files2Process:
 
-	#
-	# Open file and dump contents into memory.
-	#
+    #
+    # Open file and dump contents into memory.
+    #
 
-	print 'Processing File: ' + filePath
-	fileContents = open (filePath, 'rb').read ()
+    print 'Processing File: ' + filePath
+    fileContents = open (filePath, 'rb').read ()
 
-	#
-	# Execute actions against file contents.
-	#
+    #
+    # Execute actions against file contents.
+    #
 
-	for name, method in inspect.getmembers (actions, callable):
-		print 'Running Action: ' + name
-		fileContents = method (fileContents)
+    originalContents = str (fileContents)
+    for name, method in inspect.getmembers (actions, callable):
+        print 'Checking {0}...'.format (name),
+        fileContentsTemp = str (fileContents)
+        fileContentsTemp = method (fileContents)
 
-	#
-	# Write new file contents back to file.
-	#
+        #
+        # No problems were found if contents did not change.
+        #
 
-	fileHandle = open (filePath, 'wb')
-	fileHandle.write (fileContents)
-	fileHandle.close ()
+        if not (fileContents == fileContentsTemp):
+            foundMsg = 'Found'
+            if Fix:
+                fileContents = fileContentsTemp
+                foundMsg += ' and Fixed'
+            print foundMsg
+        else:
+            print 'Pass'
+
+    #
+    # Write new file contents back to file.
+    #
+
+    if not (originalContents == fileContents):
+        fileHandle = open (filePath, 'wb')
+        fileHandle.write (fileContents)
+        fileHandle.close ()
